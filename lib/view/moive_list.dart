8acf48d1578f0575ef4fccd9eec6d934c632c02d
zhangwx <zhangwxgo@gmail.com>
2018-05-29 15:45:47 +0800

    【update】优化代码

--- lib/view/moive_list.dart
+++ lib/view/moive_list.dart
@@ -1,32 +1,48 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class MovieList extends StatefulWidget {
-    final List movies;
-
-    MovieList(this.movies)
-
-    @override
-    _MovieListState createState() => new _MovieListState(movies);
+  @override
+  _MovieListState createState() => new _MovieListState();
 }
 
 class _MovieListState extends State<MovieList> {
-    final List movies;
+  final List movies = new List();
 
-    _MovieListState(this.movies)
+  @override
+  void initState() {
+    super.initState();
+    fetchMovie(context);
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        return new Scaffold(
-            appBar: new AppBar(title: new Text("fetch moive")),
-            body: new ListView.builder(
-                padding: EdgeInsets.zero,
-                itemCount: movies.length,
-                itemBuilder: (context, i) {
-                    final movie = movies[i];
-                    return new ListTile(
-                        title: new Text(movie['title']),
-                        subtitle: new Text(movie['original_title']));
-                }));
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(title: new Text("fetch moive")),
+        body: new ListView.builder(
+            padding: EdgeInsets.zero,
+            itemCount: movies.length,
+            itemBuilder: (context, i) {
+              final movie = movies[i];
+              return new ListTile(
+                  title: new Text(movie['title']),
+                  subtitle: new Text(movie['original_title']));
+            }));
+  }
+
+  void fetchMovie(BuildContext context) async {
+    final _jsonDecoder = new JsonDecoder();
+    final url = "https://api.douban.com/v2/movie/top250?start=0&count=10";
+    print(url);
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      print(response.statusCode);
+      Map<String, dynamic> body = _jsonDecoder.convert(response.body);
+      List subjects = body['subjects'];
+      setState(() {
+        movies.addAll(subjects);
+      });
     }
-
+  }
 }
--- lib/view/side_drawer.dart
+++ lib/view/side_drawer.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/view/moive_list.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 
 class SideDrawer extends StatefulWidget {
   @override
@@ -27,30 +25,13 @@ class _SideDrawerState extends State<SideDrawer> {
           new ListTile(
               title: new Text('fetch movie'),
               onTap: () {
-                fetchMovie(context);
+                Navigator.pop(context);
+                Navigator.push(context, new MaterialPageRoute(builder: (context) {
+                  return new MovieList();
+                }));
               })
         ],
       ),
     );
-  }
-
-  void fetchMovie(BuildContext context) async {
-    final _jsonDecoder = new JsonDecoder();
-    final url = "https://api.douban.com/v2/movie/top250?start=0&count=10";
-    print(url);
-    http.Response response = await http.get(url);
-    if (response.statusCode == 200) {
-      print(response.statusCode);
-      Map<String, dynamic> body = _jsonDecoder.convert(response.body);
-      List subjects = body['subjects'];
-      _pushMovie(context, subjects);
-    }
-  }
-
-  void _pushMovie(BuildContext context, List subject) {
-    Navigator.pop(context);
-    Navigator.push(context, new MaterialPageRoute(builder: (context) {
-      return new MovieList(subject);
-    }));
   }
 }
